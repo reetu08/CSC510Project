7d29b9fe0b226bfb332873bcd7df46db284450af
ntabass <ntabass@ncsu.edu>
2018-11-27 15:18:09 -0500

    adding comment to the code

--- scatteredness_count.py
+++ scatteredness_count.py
@@ -5,26 +5,22 @@ from collections import Counter
 import math
 import pandas as pd
 
+#calculates the scatterness of a file. the parameters are path to the file, path to repo and number of lines in the code(sloc)
 def getDeveloperScatternessOfFile(param_file_path, repo_path, sloc):
-   '''
-   output list
-   '''
    lineNoProb        = []
    lineNoCnt         = []
-   '''
-   '''
+
    cdCommand         = "cd " + repo_path + " ; "
    theFile           = os.path.relpath(param_file_path, repo_path)
-   #print 'inside the function path is',theFile
    theFile=theFile.replace('\\', '/')
    if "'" in theFile:
        return 0.0
-   #print 'the path is changed to ',theFile
    blameCommand      = " git blame -n " + theFile + " | awk '{print $2}' "
    command2Run       = cdCommand + blameCommand
-   #print 'command2run is ',command2Run
    lineNoProb        = []
 
+   #use your own bash path
+   #windows has some issues with bash, which needs the full path of bash
    blame_output      = subprocess.check_output(['C:\\cygwin64\\bin\\bash.exe','-c', command2Run])
    blame_output      = blame_output.split('\n')
    blame_output      = [x_ for x_ in blame_output if x_!='']
@@ -46,6 +42,7 @@ def getDeveloperScatternessOfFile(param_file_path, repo_path, sloc):
     
    return scatterness_cnt
 
+#rootDir is the path to repo 
 #rootDir="C:/pydriller/test-repos/puppet"
 rootDir="C:/pydriller/test-repos/gitlabhq"
 print rootDir
@@ -54,17 +51,16 @@ df = pd.DataFrame(columns=header)
 #write_file = 'C:/pydriller/resources/puppet_file_scatterness.csv'
 write_file = 'C:/pydriller/resources/GitLab_file_scatterness.csv'
 
+#this is used to walk through all the files of the repo
 for root, dirs, files in os.walk(rootDir):
     for fileName in files:
         print "fileName : ",fileName
         file_path_p=os.path.join(root,fileName)
-        #print file_path_p
         sloc = sum(1 for line in open(file_path_p))
+        #we want to exclude the image files 
         if (not '.png' in fileName) and (not '.gif' in fileName) and (not '.jpg' in fileName) and (not '.svg' in fileName):
             count= getDeveloperScatternessOfFile(file_path_p,rootDir, sloc)
             print count
-            #data=[fileName,count]
-            #data=fileName+','+str(count)
             df=df.append({'filename': fileName, 'scatterness': count},ignore_index = True)
 print df
 df.to_csv(write_file, index=False)
